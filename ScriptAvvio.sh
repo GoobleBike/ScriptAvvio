339685887638bc7522c1d09d1e2a3295684cca87
Duilio Peroni <duilio.peroni@gmail.com>
2017-07-30 09:09:21 +0200

    added delay.html

--- ScriptAvvio.sh
+++ ScriptAvvio.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo "Starting chromium in 10 seconds..."
-sleep 10
-chromium --noerrdialogs --kiosk http://localhost
+chromium-browser delay.html
+#sleep 10
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+chromium-browser --noerrdialogs --kiosk  --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble http://localhost
+
