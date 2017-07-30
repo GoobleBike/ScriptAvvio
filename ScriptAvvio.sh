fa931a57f971141226d886fa75f932365a41f8c4
Duilio Peroni <duilio.peroni@gmail.com>
2017-07-30 09:37:38 +0200

    Add files via upload

--- ScriptAvvio.sh
+++ ScriptAvvio.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Starting chromium in 10 seconds..."
-chromium-browser delay.html
 #sleep 10
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --noerrdialogs --kiosk  --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble http://localhost
-
+chromium-browser --noerrdialogs --kiosk  --no-default-browser-check --no-first-run --disable-infobars --disable-session-crashed-bubble http://localhost/delay.html
